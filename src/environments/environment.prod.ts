8dc9653c4964ed86968a900b2a2a4b786a8f5b75
Immanuel pawar <84171847+immanuelpawar4@users.noreply.github.com>
2023-04-29 11:40:38 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   model_api:'https://text2query.azurewebsites.net/api/text/text2sql?promt=${query}',
-  upload_file:"https://texttosql.azurewebsites.net/upload",
-  get_response:"https://texttosql.azurewebsites.net/sql_query_response?promt=${query}"
+  upload_file:"https://testtosqlapi.azurewebsites.net/upload",
+  get_response:"https://testtosqlapi.azurewebsites.net/sql_query_response?promt=${query}"
 };
